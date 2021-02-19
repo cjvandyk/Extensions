61174f10b9c73216f4fe7b2e2d41cebe5e042af6
Cornelius J. van Dyk <c@dtdn.com>
2021-02-19 06:00:25 -0500

    NuGet 3.0.500 release.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,18 +7,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 //[assembly: AssemblyTitle("Extensions")]
 [assembly: AssemblyTitle("Extensions.dll contains extension methods that enhance existing C# classes thus making life easier for developers." +
-                 "\nExtensions.dll supports all versions of.NET from 3.5 through 5.0" +
-                 "\nHighlights are methods such as:" +
-                 "\nUse.Save() and.Load() for easy state saving of any object in C#." +
-                 "\nUse.Retry() to easily handle 429 throttling errors in web calls." +
-                 "\nUse.IsPrime() to determine if a number is a prime number." +
-                 "\nUse.GetNthPrime() and.GetNthPrimeAsync to get the Nth prime number capable of calculating 10, 000, 000 primes / minute on an Intel Core i7 - 6700K CPU @ 4.00 GHz using 7 threads on 8 logical cores." +
-                 "\nUse.ReplaceTokens() to replace multiple strings in string dictionary style." +
-                 "\nUse.IsStrong() to validate strong passwords." +
-                 "\nUse.ToTimeZone() to quickly convert any given time to any given time zone." +
-                 "\nUse.Elevate() to restart current process in admin mode if it doesn't have admin rights." +
-                 "\nUse.CompoundInterest() to calculate any given interest over any given perior at any given interval." +
-                 "\nand many, many more.")]
+                 "\nSave(), Load() = easy state saving of any C# object," +
+                 "\nRetry() to easily handle 429 throttling errors," +
+                 "\nIsPrime(), GetNthPrime()etc.")]
 [assembly: AssemblyDescription("Extensions.dll contains extension methods that enhance existing C# classes thus making life easier for developers." +
                  "\nExtensions.dll supports all versions of.NET from 3.5 through 5.0" +
                  "\nHighlights are methods such as:" +
