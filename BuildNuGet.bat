ccf3dfc128f5ee183772ceb5b1f586e10e594bd9
Cornelius J. van Dyk <c@dtdn.com>
2023-01-14 06:28:25 -0500

    Modified BuildNuGet.bat to support Visual Studio 2022.

--- BuildNuGet.bat
+++ BuildNuGet.bat
@@ -1,8 +1,10 @@
 D:
 cd "\SOURCE\Extensions"
+rem md "bin\Debug\net6.0"
+rem copy Extensions6\bin\Debug\net6.0\Extensions6.dll bin\Debug\net6.0\Extensions6.dll /Y
 rem copy Projects\Properties\AssemblyInfo480.cs Properties\AssemblyInfo.cs /Y
 nuget pack Extensions.csproj
-move /Y Extensions*.nupkg NuGet
+rem move /Y Extensions*.nupkg NuGet
 rem cd "Projects"
 rem copy Properties\AssemblyInfo472.cs ..\Properties\AssemblyInfo.cs /Y
 rem nuget pack Extensions472.csproj
